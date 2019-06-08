bfced70c06f1eef0fbb3ce8300c9a083a276b941
Alev674642 <alev674642@gmail.com>
2019-06-08 19:49:39 +0200

    Rajout du lien vers CSS

--- src/app/post-list-item-component/post-list-item-component.component.ts
+++ src/app/post-list-item-component/post-list-item-component.component.ts
@@ -3,7 +3,8 @@ import {Post} from '../Post';
 
 @Component({
   selector: 'app-post-list-item-component',
-  templateUrl: './post-list-item-component.component.html'
+  templateUrl: './post-list-item-component.component.html',
+  styleUrls: ['./post-list-item-component.component.css']
 })
 export class PostListItemComponentComponent implements OnInit {
 
